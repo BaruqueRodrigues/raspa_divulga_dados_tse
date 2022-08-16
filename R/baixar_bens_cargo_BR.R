1d9d9cc2783aac7cd8b553dc365276e73ead8b36
Baruque Rodrigues <81967584+BaruqueRodrigues@users.noreply.github.com>
2022-08-16 19:19:10 -0300

    correção do comportamento da função

--- R/baixar_bens_cargo_BR.R
+++ R/baixar_bens_cargo_BR.R
@@ -11,18 +11,27 @@ baixar_bens_cargo_BR <- function(
           ano,
           cod_cargo){
 
-  uf <- c("AC", "AL", "AM", "AP",
-          "BA", "CE", "DF", "ES",
-          "GO", "MA", "MG", "MS",
-          "MT", "PA", "PB", "PE",
-          "PI", "PR", "RJ", "RN",
-          "RO", "RR", "RS", "SC",
-          "SE", "SP", "TO")
-  df <- purrr::map_dfr(uf, ~
-                         baixar_bens_cargo(
+  ifelse (cod_cargo ==7,
+          uf <- c("AC", "AL", "AM", "AP",
+                  "BA", "CE",  "ES",
+                  "GO", "MA", "MG", "MS",
+                  "MT", "PA", "PB", "PE",
+                  "PI", "PR", "RJ", "RN",
+                  "RO", "RR", "RS", "SC",
+                  "SE", "SP", "TO"),
+          uf <- c("AC", "AL", "AM", "AP",
+                  "BA", "CE", "DF", "ES",
+                  "GO", "MA", "MG", "MS",
+                  "MT", "PA", "PB", "PE",
+                  "PI", "PR", "RJ", "RN",
+                  "RO", "RR", "RS", "SC",
+                  "SE", "SP", "TO"))
+  df <- purrr::map_dfr(uf,
+                       purrr::possibly(~baixar_bens_cargo(
     ano = ano,
     uf = .,
-    cod_cargo = cod_cargo
+    cod_cargo = cod_cargo),
+    otherwise = NULL
   ))
 
   df
